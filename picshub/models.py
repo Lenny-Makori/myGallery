41083254824a0949d800a0dc5bac1e6ebe483728
Lenny Makori <makorilenny28@gmail.com>
2020-05-23 18:20:55 +0300

    create location model

--- picshub/models.py
+++ picshub/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class location(models.Model):
+    image_location = models.CharField(max_length=60)
+
+
+
 class Image(models.Model):
     image_name = models.CharField(max_length=60)
     image_description = models.TextField()
-    
