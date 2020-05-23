f193e686348b429f609da8405adb7a4e1fbb66cd
Lenny Makori <makorilenny28@gmail.com>
2020-05-23 14:07:08 +0300

    create image model

--- picshub/models.py
+++ picshub/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_name = models.CharField(max_length=60)
+    image_description = models.TextField()
+    
