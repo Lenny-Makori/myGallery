b2769ef82fe9befca95677d779e6c2bca7f9db6c
Lenny Makori <makorilenny28@gmail.com>
2020-05-27 19:26:14 +0300

    create test for saving image

--- picshub/tests.py
+++ picshub/tests.py
@@ -9,11 +9,20 @@ class ImageTest(TestCase):
         self.new_category = Category(category_name='vacation')
         self.new_category.save()
 
-       self.image = Image(image_name='Fashion', image_description='menswear', image_location=self.new_location, image_category=self.new_category)
+       self.image = Image(image="image1.jpg", image_name='Model', image_description='modelling', image_location=self.new_location, image_category=self.new_category)
        self.image.save_image()
 
     def tearDown(self):
         Image.objects.all().delete()
         location.objects.all().delete()
         categories.objects.all().delete()
+
+    def test_save_image(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
+
+    
+
+    
 
