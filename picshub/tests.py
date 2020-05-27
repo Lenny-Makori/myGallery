403f824d9a6fd8dbec3d84cb6f0cf4a8f5286fa2
Lenny Makori <makorilenny28@gmail.com>
2020-05-27 18:00:35 +0300

    create image test setup

--- picshub/tests.py
+++ picshub/tests.py
@@ -4,15 +4,12 @@ from .models import categories, location, Image
 # Create your tests here.
 class ImageTest(TestCase):
     def setUp(self):
-        self.new_location = location(location_name='Naivasha')
+        self.new_location = Location(location_name='Naivasha')
         self.new_location.save()
-        self.new_category = categories(category_name='vacation')
+        self.new_category = Category(category_name='vacation')
         self.new_category.save()
 
-    def tearDown(self):
-        Image.objects.all().delete()
-        location.objects.all().delete()
-        categories.objects.all().delete()
+       self.image = Image(image_name='Fashion', image_description='menswear', image_location=self.new_location, image_category=self.new_category)
+       self.image.save_image()
 
-    def test_init(self):
-        self.assertTrue(isinstance(self.image, Image))
+    
